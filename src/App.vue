5f0ee96bd4e4db53e3dfdfd2c8b5b45f687c79be
eddrickk <eddricklumine@gmail.com>
2021-06-17 19:28:57 +0700

    add navigasi halaman login

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+  <router-link to="/">Halaman Login</router-link>
+  &nbsp;
   <router-link to="/todo">Halaman Todo</router-link>
   &nbsp;
   <router-link to="/user">Halaman User</router-link>
